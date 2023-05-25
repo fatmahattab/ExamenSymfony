7f6396cda6d56c82b61da977a8624c8b8bbbfdb2
fatmahattab <114833756+fatmahattab@users.noreply.github.com>
2023-05-25 14:02:54 +0100

    Add files via upload

---
+++ src/Form/TacheType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tache;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TacheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('statut')
+            ->add('datelimite')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tache::class,
+        ]);
+    }
+}
